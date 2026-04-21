783546e6d523dd67c9fbcb93e8a819ce07e27b91
aa5974076@gmail.com <aa5974076@gmail.com>
2026-04-21 17:42:59 -0600

    fix:arreglo de formato json de las credenciales de db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const credencialesComunes = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-	port: process.env.DB_PORT || 5432
+	port: process.env.DB_PORT || 5432,
 	ssl: {
 		rejectUnauthorized: false
 	}
